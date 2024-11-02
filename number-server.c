d9b0f604306d262ade5bdc4fb8f0f8b6008034ff
kimaher <kimaher@ucsd.edu>
2024-11-02 10:15:53 +0000

    jbhm

--- number-server.c
+++ number-server.c
@@ -4,6 +4,7 @@
 int num = 0;
 
 char const HTTP_404_NOT_FOUND[] = "HTTP/1.1 404 Not Found\r\nContent-Type: text/plain\r\n\r\n";
+char const HTTP_200[] = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\n\r\n";
 
 void handle_404(int client_sock, char *path)  {
     printf("SERVER LOG: Got request for unrecognized path \"%s\"\n", path);
@@ -17,6 +18,16 @@ void handle_404(int client_sock, char *path)  {
     write(client_sock,HTTP_404_NOT_FOUND,strlen(HTTP_404_NOT_FOUND));
 }
 
+void handle_shownum(int client_sock, char *path)  {
+    char response_buff[BUFFER_SIZE];
+    snprintf(response_buff, BUFFER_SIZE, "Your number is %d\n",num);
+    // snprintf includes a null-terminator
+
+    // TODO: send response back to client
+    write(client_sock,HTTP_200,strlen(HTTP_200));
+    write(client_sock,response_buff,strlen(response_buff));
+}
+
 
 void handle_response(char *request, int client_sock) {
     char path[256];
@@ -26,6 +37,11 @@ void handle_response(char *request, int client_sock) {
     // Parse the path out of the request line (limit buffer size; sscanf null-terminates)
     if (sscanf(request, "GET %255s", path) != 1) {
         printf("Invalid request line\n");
+        return;
+    }
+
+    if(strcmp(path,"GET /shownum") == 0){
+        handle_shownum(client_sock,path);
         return;
     }
 
